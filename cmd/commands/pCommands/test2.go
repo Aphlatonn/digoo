0eaf21671db39c782336b1a028a690094a37ade1
KwaYLen.Marwane_Souiri <marwane.souiri.0@gmail.com>
2024-04-28 00:55:25 +0100

    Adding some testing commands

--- cmd/commands/pCommands/test2.go
+++ cmd/commands/pCommands/test2.go
@@ -8,14 +8,49 @@ import (
 
 func init() {
 	fmt.Println("[INFO]: command loaded - test2")
-	handler.RegisterCommand(handler.PCommand{
+	handler.RegisterPCommand(handler.PCommand{
 		Name:        "test2",
 		Description: "Test command",
 		Permissions: discordgo.PermissionAdministrator,
 		OwnerOnly:   false,
 		Run: func(session *discordgo.Session, message *discordgo.Message, args []string) {
-			// Send the message to the channel
-			session.ChannelMessageSend(message.ChannelID, "test2 command")
+			// send an embed with a button to the message channel
+			embed := &discordgo.MessageEmbed{
+				Title:       "Test Command",
+				Description: "testing the descrption baby",
+				Fields: []*discordgo.MessageEmbedField{
+					{
+						Name:  "test field",
+						Value: "- test field value",
+					},
+				},
+				Thumbnail: &discordgo.MessageEmbedThumbnail{
+					URL: message.Author.AvatarURL("1024"),
+				},
+
+				Color: 0x00ff00,
+			}
+
+			// create the button
+			button := &discordgo.Button{
+				Label:    "test button",
+				Style:    discordgo.PrimaryButton,
+				Disabled: false,
+				CustomID: "botona",
+			}
+
+			// create the message
+			session.ChannelMessageSendComplex(message.ChannelID, &discordgo.MessageSend{
+				Content: "test2 command",
+				Embeds:  []*discordgo.MessageEmbed{embed},
+				Components: []discordgo.MessageComponent{
+					discordgo.ActionsRow{
+						Components: []discordgo.MessageComponent{
+							button,
+						},
+					},
+				},
+			})
 		},
 	})
 }
