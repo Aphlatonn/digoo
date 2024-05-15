845e21a6120baa2282f82a58ba45082a1dcddf78
KwaYLen.Marwane_Souiri <marwane.souiri.0@gmail.com>
2024-05-15 18:42:53 +0100

    Adding the non prefix commands

---
+++ cmd/commands/non_prefix/init.go
@@ -0,0 +1,13 @@
+package non_prefix
+
+import (
+	_ "Digoo/cmd/commands/non_prefix/test"
+)
+
+/*
+ _______
+|       |	this file does not have any logic
+| NOTE! |	its just here to import all the non prefix commands
+|_______|	so we can make the main file cleaner
+
+*/
