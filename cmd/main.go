ce85d3a6c952e7183f20d0dabc58e30046722c6e
KwaYLen.Marwane_Souiri <marwane.souiri.0@gmail.com>
2024-05-15 18:42:15 +0100

    Cleaning the main.go file

--- cmd/main.go
+++ cmd/main.go
@@ -1,39 +1,12 @@
 package main
 
 import (
-	_ "Gooo/cmd/commands/pCommands"
-	"Gooo/cmd/events"
-	"fmt"
-	"github.com/bwmarrin/discordgo"
-	"github.com/joho/godotenv"
-	"os"
+	_ "Digoo/cmd/commands"
+	_ "Digoo/cmd/engine"
+	_ "Digoo/cmd/events"
 )
 
 func main() {
-	err := godotenv.Load()
-	// Create a new Discord session using the provided bot token
-	dg, err := discordgo.New("Bot " + os.Getenv("BOT_TOKEN"))
-	if err != nil {
-		fmt.Println("Error creating Discord session: ", err)
-		return
-	}
-
-	// ======================== Register Events ========================//
-	// Register the Ready event handler
-	dg.AddHandler(events.Ready)
-
-	// Register the MessageCreate event handler
-	dg.AddHandler(events.MessageCreate)
-	// ================================================================//
-
-	// Open a websocket connection to Discord and begin listening for events
-	err = dg.Open()
-	if err != nil {
-		fmt.Println("Error opening connection: ", err)
-		return
-	}
-
-	// Keep the bot running indefinitely
-	fmt.Println("Bot is now running. Press CTRL+C to exit.")
+	// Keep the app running indefinitely
 	<-make(chan struct{})
 }
