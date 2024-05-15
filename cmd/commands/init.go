0b8e17c900c35acf6f51cf89450bdf5bb7299d08
KwaYLen.Marwane_Souiri <marwane.souiri.0@gmail.com>
2024-05-15 18:36:48 +0100

    Adding the init file that import all the commands

---
+++ cmd/commands/init.go
@@ -0,0 +1,14 @@
+package commands
+
+import (
+	_ "Digoo/cmd/commands/non_prefix"
+	_ "Digoo/cmd/commands/prefix"
+)
+
+/*
+ _______
+|       |	this file does not have any logic
+| NOTE! |	its just here to import all the commands
+|_______|	so we can make the main file cleaner
+
+*/
